e6185f6b1d4d6cd622dbc103a88e36d8dbb6a410
Darwinmonta <montanhocaicedodarwin@gmail.com>
2026-05-06 09:34:32 -0500

    + modificaciones de cantidad de ciclos

--- ciclo-while.js
+++ ciclo-while.js
@@ -1,5 +1,10 @@
 let intento = 1
-const maximoIntentos = 3
+let ciclo = 1
+const maximoIntentos = 300000
+
+if (maximoIntentos <= 1) {
+  console.log("El número máximo de intentos debe ser almenios 1.")
+
 
 console.log("Práctica inicial con while")
 console.log("Vamos a repetir un proceso mientras el intento sea menor o igual al máximo.")
@@ -7,6 +12,7 @@ console.log("Vamos a repetir un proceso mientras el intento sea menor o igual al
 while (intento <= maximoIntentos) {
   console.log("Intento de revisión número:", intento)
   intento = intento + 1
+  ciclo = ciclo + 1
 }
 
 console.log("Proceso de revisión finalizado.")
